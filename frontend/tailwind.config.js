a276b2166c8a8be3a8fc134474c5e1bcb3182eb8
Jeff Yang <jho.yang96@gmail.com>
2021-03-15 11:01:35 +0900

    changed tailwind.config.js location

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  purge: ['/src/**/*.js', '/src/**/*.jsx', '/public/**/*.html'],
+  purge: ['./src/**/*.js', './src/**/*.jsx', './public/**/*.html'],
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {}
