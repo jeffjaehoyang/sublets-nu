2869a69b3edbc5050e4477388b40e6f965025e4c
Jeff Yang <jho.yang96@gmail.com>
2021-03-18 10:45:32 +0900

    housing list mobile style change

--- frontend/src/components/Container/HousingListCardContainer/styles.js
+++ frontend/src/components/Container/HousingListCardContainer/styles.js
@@ -3,13 +3,16 @@ import tw from 'twin.macro';
 
 export const HousingListCardContainer = styled.div`
   ${tw`flex flex-col sm:w-full md:w-7/12 md:mr-5`};
-  height: 80vh;
-  overflow: auto;
-  scrollbar-width: none;
-  -ms-overflow-style: none;
-  ::-webkit-scrollbar {
-    /* WebKit */
-    width: 0;
-    height: 0;
+  @media only screen and (min-width: 600px) {
+    height: 80vh;
+    overflow: auto;
+    scrollbar-width: none;
+    -ms-overflow-style: none;
+    ::-webkit-scrollbar {
+      /* WebKit */
+      width: 0;
+      height: 0;
+    }
   }
+  overflow: auto;
 `;
